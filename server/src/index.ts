fd1eb7466943d1ff16950bed4cd95ec3ed621d07
Phạm Leos <baoph21411@st.uel.edu.vn>
2024-10-06 04:38:29 +0700

    server modifications

--- server/src/index.ts
+++ server/src/index.ts
@@ -34,7 +34,7 @@ app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
 
 /* SERVER */
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
 });
